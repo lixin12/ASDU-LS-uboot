ed8c0e75617793ec1598ddd0fc826eb8cd380cfd
lixin12 <1173057975@qq.com>
2020-07-13 09:45:12 +0000

    修改gcc路径

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 make distclean
-export CROSS_COMPILE=../gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+export CROSS_COMPILE=/usr/local/arm/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 export ARCH=arm
 
 make mx6ull_14x14_evk_defconfig
